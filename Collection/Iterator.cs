09702d2c7bf7c9b31aa9c82f687bdad92f63a5e9
Andrew Minich <andrew.minich1@gmail.com>
2015-03-24 16:55:53 +0300

    ASP.NET.Day8

---
+++ Collection/Iterator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    public static class IteratorLogic
+    {
+
+        public static IEnumerable<T> Iterator<T>(this IEnumerable<T> collection, Func<T, bool> func)
+        {
+            if (ReferenceEquals(collection, null) || ReferenceEquals(func,null)) throw new ArgumentNullException();
+            T[] array = collection.ToArray<T>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (func(array[i]) == true) yield return array[i];
+            }
+        }
+
+        public static IEnumerable<T> Iterator2<T>(this IEnumerable<T> collection,int number, Func<int, int> func)
+        {
+            if (ReferenceEquals(collection, null) || ReferenceEquals(func, null)) throw new ArgumentNullException();
+            T[] array = collection.ToArray<T>();
+            int i = number;
+            while(i<array.Length && i>=0)
+            {
+                yield return array[i];
+                i = func(i);
+            }
+        }
+    }
+}
---
+++ CollectionConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Collection;
+
+namespace CollectionConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+             int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6 };
+            char[] arrChar = new char[] { 'a', 'B', 'C', 'd', 'E' };
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            string str =  "aB12lhv78gd";
+            
+            foreach(var x in arr2.Iterator2<int>(0,(i)=>{return i+2;}))
+            {
+                Console.Write(x);
+            }
+
+            Console.WriteLine();
+
+            foreach(var x in list.Iterator<int>((i)=>{return i>3;}))
+            {
+                Console.Write(x);
+            }
+            Console.WriteLine();
+
+            foreach (var x in arr.Iterator<int>((i) => { return i % 2==0; }))
+            {
+                Console.Write(x);
+            }
+
+            Console.WriteLine();
+
+            foreach (var x in arrChar.Iterator<char>((i) => { return Char.IsLower(i) == true; }))
+            {
+                Console.Write(x);
+            }
+
+            Console.WriteLine();
+
+            foreach (var x in str.Iterator<char>((i) => { return Char.IsDigit(i) == true; }))
+            {
+                Console.Write(x);
+            }
+
+            Console.WriteLine();
+
+            
+        }
+    }
+}
